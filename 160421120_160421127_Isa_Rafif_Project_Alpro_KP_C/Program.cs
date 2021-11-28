f8cf918d135d0a2dcfc5a5203eed550fdb0949d6
Aorus <Aorus@DESKTOP-PTHM1HI>
2021-11-28 17:58:57 +0700

    Add project files.

---
+++ 160421120_160421127_Isa_Rafif_Project_Alpro_KP_C/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _160421120_160421127_Isa_Rafif_Project_Alpro_KP_C
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
